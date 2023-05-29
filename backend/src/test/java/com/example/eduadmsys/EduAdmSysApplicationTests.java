9fc1dfdb77f40732ebaf4b827434bcc519165884
Zhonghuan Wang <3361417053@qq.com>
2023-05-29 21:49:12 +0800

    Initial commit

---
+++ backend/src/test/java/com/example/eduadmsys/EduAdmSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eduadmsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAdmSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
